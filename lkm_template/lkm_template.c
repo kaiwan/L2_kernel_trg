d7de86221d098ecd3612b2532c46eaf8521ae974
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2019-03-16 17:19:40 +0530

    lkm_template: initial commit
    Signed-off-by: Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>

---
+++ lkm_template/lkm_template.c
@@ -0,0 +1,32 @@
+/*
+ * lkm_template/lkm_template.c
+ ***************************************************************
+ * < Your desc, comments, etc >
+ * (c) Author:
+ ****************************************************************
+ * Brief Description:
+ *
+ */
+#include <linux/init.h>
+#include <linux/module.h>
+
+#define OURMODNAME   "lkm_template"
+
+MODULE_AUTHOR("<insert your name here>");
+MODULE_DESCRIPTION("a hello, world type LKM");
+MODULE_LICENSE("Dual MIT/GPL");
+MODULE_VERSION("0.1");
+
+static int __init lkm_template_init(void)
+{
+	pr_debug("%s: inserted\n", OURMODNAME);
+	return 0;		/* success */
+}
+
+static void __exit lkm_template_exit(void)
+{
+	pr_debug("%s: removed\n", OURMODNAME);
+}
+
+module_init(lkm_template_init);
+module_exit(lkm_template_exit);
