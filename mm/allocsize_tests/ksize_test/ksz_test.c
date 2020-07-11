615f25ba3188c12810353b5cf2d5d5358c556279
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2020-07-11 13:04:42 +0530

    fmt cleanup

--- mm/allocsize_tests/ksize_test/ksz_test.c
+++ mm/allocsize_tests/ksize_test/ksz_test.c
@@ -1,17 +1,17 @@
 /*
  * ksz_test.c
- * A quick kmalloc/ksize test; we allocate kernel RAM with kmalloc(), 
+ * A quick kmalloc/ksize test; we allocate kernel RAM with kmalloc(),
  * starting with 100 bytes and incrementing this in a loop by 'stepsz'
  * bytes (you can vary it by passing it as a module parameter).
  * Thus, this kernel module clearly shows how kernel memory can get wasted
  * by using poorly thought-out amounts.
  *
- * Note- the kernel module will probably appear to fail on insmod with a 
+ * Note- the kernel module will probably appear to fail on insmod with a
  * message similar to:
-# insmod ./ksz_test.ko 
+# insmod ./ksz_test.ko
 insmod: ERROR: could not insert module ./ksz_test.ko: Cannot allocate memory
-# 
- * This is expected- we stress the system asking for larger and larger chunks 
+#
+ * This is expected- we stress the system asking for larger and larger chunks
  * of kernel direct-mapped RAM via the kmalloc, so ultimately it will fail.
  *
  * Please LOOK UP the kernel ring buffer via 'dmesg' to see the printk's!
@@ -25,21 +25,24 @@ insmod: ERROR: could not insert module ./ksz_test.ko: Cannot allocate memory
 
 MODULE_LICENSE("Dual MIT/GPL");
 MODULE_AUTHOR("Kaiwan NB, kaiwanTECH");
-MODULE_DESCRIPTION("A quick kmalloc/ksize test; shows how kernel memory can get wasted \
-by using poorly thought-out amounts");
+MODULE_DESCRIPTION
+("A quick kmalloc/ksize test; shows how kernel memory can get wasted"
+" by using poorly thought-out amounts");
 
-static int stepsz=10000;
+static int stepsz = 10000;
 module_param(stepsz, int, 0);
-MODULE_PARM_DESC(stepsz, "Number of bytes to increment each kmalloc attempt by (default=10000)");
+MODULE_PARM_DESC(stepsz,
+		 "Number of bytes to increment each kmalloc attempt by (default=10000)");
 
-const char *hdr=
+const char *hdr =
 "[1] kmalloc(n)   :[2]actual: wastage : %age\n"
 "      bytes      :   bytes :  [2-1]  : waste\n";
 
 static int __init kmt_init(void)
 {
-	int num=100, actual_alloc=0;
+	int num = 100, actual_alloc = 0;
 	void *p;
+
 	pr_info("%s", hdr);
 
 	while (1) {
@@ -50,11 +53,12 @@ static int __init kmt_init(void)
 		}
 		actual_alloc = ksize(p);
 		pr_info("kmalloc(%7d) : %7d : %7d : %3d%%\n",
-			num, actual_alloc, actual_alloc-num, (((actual_alloc-num)*100/num)));
+			num, actual_alloc, actual_alloc - num,
+			(((actual_alloc - num) * 100 / num)));
 		kfree(p);
 		num += stepsz;
 	}
-	return 0; // success
+	return 0;		// success
 }
 
 static void __exit kmt_exit(void)
