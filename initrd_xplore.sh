cc0e2d655eb3be4a11fd24ea75117d46338b1123
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2020-09-11 13:33:32 +0530

    kbuild: initial commit

---
+++ initrd_xplore.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# initrd_xplore.sh
+# 
+# Quick Description:
+# 
+# 
+# Last Updated :
+# Created      :
+# 
+# Author:
+# Kaiwan N Billimoria
+# kaiwan -at- kaiwantech -dot- com
+# kaiwanTECH
+# 
+# License:
+# GPL / LGPL
+# 
+name=$(basename $0)
+source ./common.sh || {
+ echo "$name: could not source common.sh , aborting..."
+ exit 1
+}
+
+########### Functions follow #######################
+
+
+
+main()
+{
+TMPDIR=tmp.$$
+DEST=initrd_copy.img
+DEST_DIR=initrd_content
+
+mkdir -p ${TMPDIR} || exit 1
+cp $1 ${TMPDIR}/${DEST} || exit 1
+cd ${TMPDIR}
+mv ${DEST} ${DEST}.gz
+gzip -d ${DEST}.gz
+mkdir -p ${DEST_DIR}
+cd ${DEST_DIR}
+cpio -i < ../${DEST}
+}
+
+##### execution starts here #####
+
+# args
+[ $# -ne 1 ] && {
+  echo "Usage: ${name} initrd-img-file"
+  exit 1
+}
+[ ! -f $1 ] && {
+  echo "${name}: initrd-img-file $1 not found? Perms issue? Aborting..."
+  exit 1
+}
+INITRD_SRC=$1
+main ${INITRD_SRC}
+exit 0
