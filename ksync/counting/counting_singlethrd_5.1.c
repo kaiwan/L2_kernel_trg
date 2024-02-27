6886ad690363c1c79044c878cf3bb9d6671519ca
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2024-02-27 12:32:52 +0530

    ksync new counting code

---
+++ ksync/counting/counting_singlethrd_5.1.c
@@ -0,0 +1,34 @@
+/*
+ * Src: Is Parallel Programming Hard, And, If So, What Can You Do About It? Paul McKenny
+ * The single-threaded version - this one - works fine!
+ * Not so, the multithreaded one: see and try counting_5.1.c
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+unsigned long long counter = 0;
+
+static __inline__ void inc_count(void)
+{
+	counter++;
+	//WRITE_ONCE(counter, READ_ONCE(counter) + 1);
+}
+
+static __inline__ unsigned long long read_count(void)
+{
+	return counter;
+	//return READ_ONCE(counter);
+}
+
+unsigned long long COUNT=1000000000L; // 10^9 // (10*1024*1024*1024)L;
+
+int main()
+{
+	unsigned long long i;
+
+	for (i = 0; i < COUNT; i++)
+		inc_count();
+	printf("COUNT=      %llu\ncounter val=%llu\n", COUNT, read_count());
+	exit(0);
+}
