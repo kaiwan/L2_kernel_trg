5917851036556bdc5f354580ffe82a9b791d7948
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2025-07-29 08:55:44 +0530

    core_running_what.sh: dyn generate the script file
    Signed-off-by: Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>

--- sched/core_running_what.sh
+++ sched/core_running_what.sh
@@ -2,26 +2,35 @@
 # core_running_what.sh
 name=$(basename $0)
 
+# This is how it should be... so we generate the funcs!
+[[ 0 -eq 1 ]] && {
 # Show thread(s) running on cpu core 'n'  - func c'n'
 function c0()
 {
 ps -eLF |awk '{ if($5==0) print $0}'
 }
-function c1()
-{
-ps -eLF |awk '{ if($5==1) print $0}'
-}
-function c2()
-{
-ps -eLF |awk '{ if($5==2) print $0}'
-}
-function c3()
-{
-ps -eLF |awk '{ if($5==3) print $0}'
-}
 
 # "main" ..
-echo "Run this script as
- source ${name}
-and then run the builtin functions 'c0', 'c1', 'c2', ...
+
+numcores=$(($(nproc)-1))
+[[ ${numcores} -le 0 ]] && numcores=1
+
+SOURCED_SCRIPT=corefuncs
+echo "Generating script \"${SOURCED_SCRIPT}\" for CPU cores 0 to ${numcores} now..."
+rm -f ${SOURCED_SCRIPT}
+for core in $(seq 0 ${numcores})
+do
+  #echo "core=${core}"
+  echo "function c${core}()
+{
+ps -eLF |awk '{ if(\$5==${core}) print \$0}'
+}" >> ${SOURCED_SCRIPT}
+
+done
+echo "Done.
+
+To run:
+Step 1. source ./${SOURCED_SCRIPT}
+Step 2. run the builtin functions 'c0', 'c1', 'c2', ...
 to see which threads are running on which core."
+exit 0
